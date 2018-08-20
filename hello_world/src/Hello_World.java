5c6b4fb9366783544b96d75cd485cf1544597581
APCS3rd <APCS3rd@MHS-Dreyer-15>
2018-08-20 10:47:23 -0700

    First commit to github.

---
+++ hello_world/src/Hello_World.java
@@ -0,0 +1,15 @@
+/**
+ * Prints message in console
+ * 
+ * @author Alexis Lin 
+ * @version 8/17/18
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+    	 System.out.println("Hello World! From Alexis"); 
+    	 System.out.println(":)"); //Display the string
+    }
+}
+
