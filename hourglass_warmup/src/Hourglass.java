874a8c5d010e3d12fb329b11b65894c896925659
linalek6 <42460667+linalek6@users.noreply.github.com>
2018-10-23 21:30:30 -0700

    10/23 ni

--- hourglass_warmup/src/Hourglass.java
+++ hourglass_warmup/src/Hourglass.java
@@ -5,17 +5,23 @@ public class Hourglass {
 		printBase();
 		printTopHalf();
 		System.out.println("     ||");
+		printBottomHalf();
+		printBase();
 	}
 	public static void printBase() {
-		System.out.print("|");
-		for (int i = 0; i  < 10; i++) {
+		for (int i = 0; i  < 12; i++) {
+			if (i==0 || i ==11) {
+				System.out.print("|");
+			}
+			else {
 			System.out.print("\"");
+			}
 		}
-		System.out.print("|");
+		System.out.println(); 
 	}
 	public static void printTopHalf() {
 		for (int i = 8; i >= 2; i -= 2) {
-			System.out.println();
+			//System.out.println();
 			for (int x = 8; x >= i; x -=2) {
 				System.out.print(" ");
 			}
@@ -23,18 +29,21 @@ public class Hourglass {
 			for (int n = 1; n <= i; n++) {
 				System.out.print(":");
 			}
-			System.out.print("/");
+			System.out.println("/");
+			
 		}
-		System.out.println(); 
+		//System.out.println(); 
 	}
 	public static void printBottomHalf() {
 		for (int i = 2; i <= 8; i+= 2) {
-			System.out.println();
-			for (int x = 2; x <= i; x += 2) {
+			for (int x = 8; x >= i; x -= 2) {
 				System.out.print(" ");
 			}
 			System.out.print("/");
-			for (int n = 1; n <= 2
+			for (int n = 1; n <= i; n++) {
+				System.out.print(":");
+			}
+			System.out.println("\\");
 		}
 	}
 }
