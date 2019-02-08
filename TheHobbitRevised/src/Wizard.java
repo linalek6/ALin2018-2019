0a7ec4e8b3ac0e8c4f58e22eb88b9c15870525ce
APCS3rd <APCS3rd@MHS-Dreyer-15>
2019-02-08 10:55:21 -0800

    first commit

---
+++ TheHobbitRevised/src/Wizard.java
@@ -0,0 +1,32 @@
+
+public class Wizard extends Traveler
+{
+	//  Ex.  Calling travel(50) would increase a Wizard's distance traveled by 150.
+	//Wizards have colors associated with them.  Saruman the White, Gandalf the Grey, etc
+	//  Override the Traveler's getName method to append the Wizard's color 
+	
+	private String color;
+	
+	//Call Traveler's constructor (look it up p587) to set the name field and then 
+		//   set the color field for this class   
+	public Wizard(String name, String color)
+	{
+		super(name);
+		this.color = color;
+	
+	}
+	//Wizards travel 3 times as far as other travelers in their party because they scout ahead.
+	//  Override the travel method, calling the method from the superclass and passing it
+	//  triple the miles passed in as a parameter.
+	//  Ex.  Calling travel(50) would increase a Wizard's distance traveled by 150.
+	public void travel(int miles)
+	{
+		super.travel(miles*3);
+		
+	}
+	
+	public String getName()
+	{
+		
+	}
+}
